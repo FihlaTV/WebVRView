7d9f2789175b3b7652f627a964fc066b0bd3c81e
Richard Li <lichard49@gmail.com>
2018-01-26 01:37:24 -0500

    Introduce Java to Javascript interaction

--- app/src/main/java/com/lichard49/webvr/MainActivity.java
+++ app/src/main/java/com/lichard49/webvr/MainActivity.java
@@ -3,19 +3,25 @@ package com.lichard49.webvr;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.content);
+        webView = findViewById(R.id.content);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccessFromFileURLs(true); //Maybe you don't need this rule
@@ -23,6 +29,18 @@ public class MainActivity extends Activity {
         webSettings.setUseWideViewPort(true);
         webView.loadUrl("file:///android_asset/index.html");
 //        webView.loadUrl("http://aframe.glitch.me/");
-
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url){
+                webView.loadUrl("javascript:alert(setColor('" + "#000000" + "'))");
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Log.d("lichard49", message);
+                result.confirm();
+                return true;
+            }
+        });
     }
 }
