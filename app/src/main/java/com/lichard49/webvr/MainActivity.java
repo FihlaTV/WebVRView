ad43931c2c3b4c5d1930d60e8eb59e91769e491f
Richard Li <lichard49@gmail.com>
2018-01-26 10:42:27 -0500

    Added tap interaction to iterate through choices

--- app/src/main/java/com/lichard49/webvr/MainActivity.java
+++ app/src/main/java/com/lichard49/webvr/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.JsResult;
@@ -12,9 +14,11 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnTouchListener {
 
     private WebView webView;
+    private int choiceIndex = 1;
+    private boolean webViewLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.content);
+        webView.setOnTouchListener(this);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccessFromFileURLs(true); //Maybe you don't need this rule
@@ -31,7 +36,7 @@ public class MainActivity extends Activity {
 //        webView.loadUrl("http://aframe.glitch.me/");
         webView.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView view, String url){
-                webView.loadUrl("javascript:alert(setColor('" + "#000000" + "'))");
+                webViewLoaded = true;
             }
         });
         webView.setWebChromeClient(new WebChromeClient() {
@@ -42,5 +47,15 @@ public class MainActivity extends Activity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            choiceIndex = (choiceIndex + 1) % 3;
+            Log.d("lichard49", "POKE " + choiceIndex);
+            webView.loadUrl("javascript:selectChoice('" + choiceIndex + "')");
+        }
+        return false;
     }
 }
